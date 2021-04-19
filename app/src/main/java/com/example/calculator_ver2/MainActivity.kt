65a2ef6d3f9af75fb3a891f4e19e689a9cdaf749
USER <gusanans218>
2021-04-19 18:26:43 +0900

    hihidddddd

---
+++ app/src/main/java/com/example/calculator_ver2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.calculator_ver2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun buttonClicked(v: View){
+ddddd
+    }
+}
